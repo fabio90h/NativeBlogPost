d1ed00cc4d3c5c1454ca40a8fda63e616a9aace5
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2020-04-13 08:42:34 -0700

    Wiring Up Context

--- App.js
+++ App.js
@@ -2,16 +2,19 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import IndexScreen from "./src/screens/IndexScreen";
+import { BlogProvider } from "./src/context/BlogContext";
 
 const Stack = createStackNavigator();
 
 const App = () => {
 	return (
-		<NavigationContainer>
-			<Stack.Navigator initialRouteName="Index">
-				<Stack.Screen name="Index" component={IndexScreen} />
-			</Stack.Navigator>
-		</NavigationContainer>
+		<BlogProvider>
+			<NavigationContainer>
+				<Stack.Navigator initialRouteName="Index">
+					<Stack.Screen name="Index" component={IndexScreen} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		</BlogProvider>
 	);
 };
 
