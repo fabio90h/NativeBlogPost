11ff3adc6aca52218071d14352928a6bb935af90
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2020-04-14 08:17:19 -0700

    Adding styles to the rows of blogs

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = () => {
 	const { state, addBlogPost } = useContext(Context);
@@ -13,13 +14,33 @@ const IndexScreen = () => {
 				data={state}
 				keyExtractor={(current) => current.title}
 				renderItem={({ item }) => {
-					return <Text>{item.title}</Text>;
+					return (
+						<View style={styles.row}>
+							<Text style={styles.title}>{item.title}</Text>
+							<Feather style={styles.icon} name="trash" />
+						</View>
+					);
 				}}
 			/>
 		</View>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	row: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		paddingVertical: 10,
+		paddingHorizontal: 5,
+		borderColor: "gray",
+		borderTopWidth: 1,
+	},
+	icon: {
+		fontSize: 24,
+	},
+	title: {
+		fontSize: 18,
+	},
+});
 
 export default IndexScreen;
